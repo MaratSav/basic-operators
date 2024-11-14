04178024bf3c0fc23efecd765cddee6bc6710536
MaratSav <marat1620@gmail.com>
2024-11-14 20:24:52 +0300

    скрин поток выполнен проги

---
+++ module_2_3.py
@@ -0,0 +1,10 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while index < len(my_list):
+    print(my_list[index])
+    index += 1
+    my_int = my_list[index]
+    if my_int >= 0:
+        continue
+    else:
+        break
